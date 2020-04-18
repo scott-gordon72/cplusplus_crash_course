1e0b1e990fd06b2ca97091eea9d0c1e7addef49e
scott <scott.gordon72@outlook.com>
2020-04-18 16:37:39 -0400

    Add 2-16, 2-17 and 2-18

---
+++ chapter_2/2-16.cpp
@@ -0,0 +1,20 @@
+/*
+ * Example using the POD type Book to read and write members
+ */
+
+#include <cstdio>
+
+struct Book
+{
+    char name[256];
+    int year;
+    int pages;
+    bool hardcover;
+};
+
+int main()
+{
+    Book neuromancer;
+    neuromancer.pages = 271;
+    printf("Neuromancer has %d pages. ", neuromancer.pages);
+}
---
+++ chapter_2/2-17.cpp
@@ -0,0 +1,9 @@
+/*
+ * An example union
+ */
+
+union Variant {
+    char string[10];
+    int integer;
+    double floating_point;
+};
