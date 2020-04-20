6c8cc978fcd32ca061271872992508a0816a3012
scott <scott.gordon72@outlook.com>
2020-04-20 00:08:37 -0400

    Finished for the night

---
+++ chapter_2/2-20.cpp
@@ -0,0 +1,21 @@
+struct ClockOfTheLongNow
+{
+    void add_year()
+    {
+        year++;
+    }
+    bool set_year(int new_year)
+    {
+        if (new_year < 2019)
+            return false;
+        year = new_year;
+        return true;
+    }
+    int get_year()
+    {
+        return year;
+    }
+
+private:
+    int year;
+};
